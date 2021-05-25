fee729e8bedbbcdd682a645986d1629dbb82dba6
Albert Jonsson <j.albert.jonsson@gmail.com>
2021-05-25 11:39:40 +0200

    initial commit

--- model.py
+++ model.py
@@ -1,76 +1,28 @@
+from torchvision import models
+from PIL import Image
 import torch
-import torch.nn as nn
-import torchvision.transforms.functional as TF
+import matplotlib.pyplot as plt
+import torchvision.transforms as T
+import numpy as np
 
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(DoubleConv, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-        )
 
-    def forward(self, x):
-        return self.conv(x)
+fcn = models.resnet34(pretrained=True).eval()
+img = Image.open("bird.png")
 
-class UNET(nn.Module):
-    def __init__(
-            self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],
-    ):
-        super(UNET, self).__init__()
-        self.ups = nn.ModuleList()
-        self.downs = nn.ModuleList()
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+plt.imshow(img); plt.show()
 
-        # Down part of UNET
-        for feature in features:
-            self.downs.append(DoubleConv(in_channels, feature))
-            in_channels = feature
+trf = T.Compose([T.Resize(256),
+                 T.CenterCrop(224),
+                 T.ToTensor()])
+                 #T.Normalize(mean = [0.485, 0.456, 0.406],
+                 #            std = [0.229, 0.224, 0.225])]
 
-        # Up part of UNET
-        for feature in reversed(features):
-            self.ups.append(
-                nn.ConvTranspose2d(
-                    feature*2, feature, kernel_size=2, stride=2,
-                )
-            )
-            self.ups.append(DoubleConv(feature*2, feature))
+print(img.size)
+inp = trf(img).unsqueeze(0)
+print(inp.shape)
+out = fcn(inp)['out']
+print(out.shape)
 
-        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
-        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
-
-    def forward(self, x):
-        skip_connections = []
-
-        for down in self.downs:
-            x = down(x)
-            skip_connections.append(x)
-            x = self.pool(x)
-
-        x = self.bottleneck(x)
-        skip_connections = skip_connections[::-1]
-
-        for idx in range(0, len(self.ups), 2):
-            x = self.ups[idx](x)
-            skip_connection = skip_connections[idx//2]
-
-            if x.shape != skip_connection.shape:
-                x = TF.resize(x, size=skip_connection.shape[2:])
-
-            concat_skip = torch.cat((skip_connection, x), dim=1)
-            x = self.ups[idx+1](concat_skip)
-
-        return self.final_conv(x)
-
-def test():
-    x = torch.randn((3, 1, 161, 161))
-    model = UNET(in_channels=1, out_channels=1)
-    preds = model(x)
-    assert preds.shape == x.shape
-
-if __name__ == "__main__":
-    test()
+om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
+print(om.shape)
+print(np.unique(om))
